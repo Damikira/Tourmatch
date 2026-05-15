6e76f20bd6141702faeab0b011e370ea6deddd92
Damikira <da.vergarao@duocuc.cl>
2026-05-15 11:51:30 -0400

    Add files via upload

---
+++ Proyecto/Backend/src/main/java/com/tourmatch/backend/controllers/VehiculoController.java
@@ -0,0 +1,29 @@
+package com.tourmatch.backend.controllers;
+
+import com.tourmatch.backend.dtos.VehiculoRequest;
+import com.tourmatch.backend.models.Vehiculo;
+import com.tourmatch.backend.services.VehiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/vehiculos")
+@CrossOrigin(origins = "http://localhost:5173") // <-- ESTO ES VITAL
+public class VehiculoController {
+
+    @Autowired
+    private VehiculoService vehiculoService;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrarVehiculo(@RequestBody VehiculoRequest request, Principal principal) {
+        try {
+            Vehiculo nuevoVehiculo = vehiculoService.registrarVehiculo(request, principal.getName());
+            return ResponseEntity.ok(nuevoVehiculo);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
